84121ae48a0bab52b09d89cde48cf43309e56235
Sandbox Group <brian@makemysandbox.com>
2026-03-25 18:43:33 -0700

    Add Express server for production

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(PORT, '0.0.0.0', function() {
+  console.log('Forge Intelligence workspace running on port ' + PORT);
+});
